d2374b5233404575c3d3af54ea39ae5d6375210b
kshravi86 <kshravi86@gmail.com>
2018-10-21 15:39:12 +0530

    cucjcuc

--- js/admob.js
+++ js/admob.js
@@ -1,8 +1,8 @@
 var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
-        banner: 'ca-app-pub-5830283354936773/3267060316',
-       interstitial: 'ca-app-pub-5830283354936773/6502941202'
+        banner: 'ca-app-pub-5170973579111533/1085713519',
+       interstitial: 'ca-app-pub-5170973579111533/8936283376'
 	   
 	   
     };
